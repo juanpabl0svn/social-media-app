2b63eca8f1d391c3553ba0ffb11cc9e17a076ed1
JuanK <juancamilo.manjarres@gmail.com>
2024-03-08 10:46:58 -0500

    Revert "feat: login form"
    This reverts commit f958acc97eebcb9984f18af188cdd29429d28acc.

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,20 +1,12 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css',
+  styleUrl: './login.component.css'
 })
 export class LoginComponent {
-  loginForm = new FormGroup({
-    passwordControl : new FormControl(''),
-    usernameControl : new FormControl(''),
-  })
-  onSubmit() {
-    // TODO: Use EventEmitter with form value
-    console.log(this.loginForm.value);
-  }
+
 }
