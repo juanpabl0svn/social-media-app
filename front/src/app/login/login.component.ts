f958acc97eebcb9984f18af188cdd29429d28acc
JuanK <juancamilo.manjarres@gmail.com>
2024-03-08 10:43:03 -0500

    feat: login form

--- front/src/app/login/login.component.ts
+++ front/src/app/login/login.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
-
+  loginForm = new FormGroup({
+    passwordControl : new FormControl(''),
+    usernameControl : new FormControl(''),
+  })
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.log(this.loginForm.value);
+  }
 }
