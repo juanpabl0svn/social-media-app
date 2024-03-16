a586fc61433f3325a516223ecce0c9c106a0a0f9
JuanK <juancamilo.manjarres@gmail.com>
2024-03-16 01:54:19 -0500

    feat: Database and tables creation scripts

---
+++ db-scripts/createTables.sql
@@ -0,0 +1,59 @@
+CREATE TABLE users (
+	id_user INT PRIMARY KEY AUTO_INCREMENT,
+    nickname VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255) NOT NULL,
+	last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    birth_date TIMESTAMP NULL DEFAULT NULL
+);
+
+CREATE TABLE posts (
+	id_post INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    uri_resource VARCHAR(255) NOT NULL,
+    post_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    title VARCHAR(255),
+    FOREIGN KEY (id_user) REFERENCES users(id_user)
+);
+
+CREATE TABLE likes (
+	id_like INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_post INT,
+    emoji_like BOOLEAN NOT NULL,
+    like_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_post) REFERENCES posts(id_post)
+);
+
+CREATE TABLE followers (
+	id_follow INT PRIMARY KEY AUTO_INCREMENT,
+    id_user_request INT,
+    id_user_follow INT,
+    state VARCHAR(255) NOT NULL,
+    request_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    request_update_date TIMESTAMP NULL DEFAULT NULL,
+    FOREIGN KEY (id_user_request) REFERENCES users(id_user),
+    FOREIGN KEY (id_user_follow) REFERENCES users(id_user)
+);
+
+CREATE TABLE comments (
+	id_comment INT PRIMARY KEY AUTO_INCREMENT,
+    id_user INT,
+    id_post INT,
+    comment VARCHAR(255) NOT NULL,
+    comment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user) REFERENCES users(id_user),
+    FOREIGN KEY (id_post) REFERENCES posts(id_post)
+);
+
+CREATE TABLE direct_messages (
+	id_message INT PRIMARY KEY AUTO_INCREMENT,
+    id_user_from INT,
+    id_user_to INT,
+    message VARCHAR(255) NOT NULL,
+    message_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (id_user_from) REFERENCES users(id_user),
+    FOREIGN KEY (id_user_to) REFERENCES users(id_user)
+);
