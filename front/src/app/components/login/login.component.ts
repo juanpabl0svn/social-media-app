41dbe3cad0de6b2a775cb5abc5cd498e50089064
JuanK <juancamilo.manjarres@gmail.com>
2024-03-20 19:44:41 -0500

    x

---
+++ front/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { DataService } from '../../services/data.service';
+import { RouterLink } from '@angular/router';
+import { BackendReqService } from '../../services/backendReq/backend-req.service';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css',
+})
+export class LoginComponent {
+  loginForm = new FormGroup({
+    passwordControl: new FormControl(''),
+    usernameControl: new FormControl(''),
+  });
+
+  constructor(public dataService: DataService, public backendReqService: BackendReqService) {}
+
+  onSubmit() {
+    const { usernameControl: username, passwordControl: password } =
+      this.loginForm.value;
+
+    this.dataService.setValues({ username, password });
+    this.dataService.print();
+
+    this.backendReqService.logInReq(username, password)?.subscribe(response => console.log(response))
+  }
+}
