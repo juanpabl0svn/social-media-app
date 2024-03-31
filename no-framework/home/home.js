0722ab9e7dca9308b1bd5b75e0ee7f5c65bdc8b3
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-03-31 15:24:23 -0500

    js in home to make it real

--- no-framework/home/home.js
+++ no-framework/home/home.js
@@ -11,3 +11,21 @@ function logOut() {
     }
   });
 }
+
+window.onload = () => {
+  const posts = $("#user-posts");
+  let i;
+  for (i = 1; i <= 4; i++) {
+    const image = `<img src='assets/post${i}.jpg'/>`;
+    posts.insertAdjacentHTML("beforeend", image);
+  }
+
+  $("#posts-number").innerText = i - 1;
+
+  if (i != 0) $("#first-post").style.display = "none";
+
+  $("#friends-number").innerText = Math.floor(Math.random() * 1000);
+
+  const userImage = $("#user-image");
+  userImage.src = "assets/user_logged_in.jpg";
+};
