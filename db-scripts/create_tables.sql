f1c93a76a14cd2d0888f95d31768133ff0e74257
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-04-01 22:03:08 -0500

    done

--- db-scripts/create_tables.sql
+++ db-scripts/create_tables.sql
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS users (
     last_name VARCHAR(255) NOT NULL,
     password VARCHAR(255) NOT NULL,
     email VARCHAR(255) UNIQUE NOT NULL,
-    create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-    birth_date DATE NOT NULL
+    create_date TIMESTAMP DEFAULT NOW(),
+    birth_date TIMESTAMP NOT NULL
 );
 
 -- ?
