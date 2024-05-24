582efa612755140dcf131a64889183a7b6ccf1e1
JuanK <juancamilo.manjarres@gmail.com>
2024-05-23 23:35:27 -0500

    feat: search form added

--- front/src/app/components/search/search.component.ts
+++ front/src/app/components/search/search.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-search',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './search.component.html',
   styleUrl: './search.component.css'
 })
 export class SearchComponent {
+  usersList:any = []
 
+  searchForm = new FormGroup({
+    search_text: new FormControl('')
+  })
+
+  onSubmit(e:Event){
+    e.preventDefault()
+  }
 }
