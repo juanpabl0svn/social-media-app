d1a83f214690ed51f9e5f9e1ca7841e15b056d27
JuanK <juancamilo.manjarres@gmail.com>
2024-05-24 21:23:57 -0500

    refactor: firebase credentials to environment

--- back/src/db.firebase.ts
+++ back/src/db.firebase.ts
@@ -1,22 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase, ref, set, update, remove } from "firebase/database";
 import { getStorage } from "firebase/storage";
+import { FIREBASE_APIKEY, FIREBASE_APPID, FIREBASE_AUTHDOMAIN, FIREBASE_MESSAGESENDERID, FIREBASE_PROJECTID, FIREBASE_STORAGEBUCKET } from "./config";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA4os8fgAF67z19gvmANUq6aFdtGYgZMwQ",
-  authDomain: "desarrolloweb-b9169.firebaseapp.com",
-  projectId: "desarrolloweb-b9169",
-  storageBucket: "desarrolloweb-b9169.appspot.com",
-  messagingSenderId: "743373544653",
-  appId: "1:743373544653:web:44a6d76f831ca5bf881d84"
-  // apiKey: "AIzaSyAoCr5_tGtB1FUPO5XPsje2eST4OO4Ib5E",
-  // authDomain: "firtsnosqldb.firebaseapp.com",
-  // databaseURL: "https://firtsnosqldb-default-rtdb.firebaseio.com",
-  // projectId: "firtsnosqldb",
-  // storageBucket: "firtsnosqldb.appspot.com",
-  // messagingSenderId: "541707749750",
-  // appId: "1:541707749750:web:7baa2e5e3e8fae94031257",
-  // measurementId: "G-8G9VRLYDZM",
+  apiKey: FIREBASE_APIKEY,
+  authDomain: FIREBASE_AUTHDOMAIN,
+  projectId: FIREBASE_PROJECTID,
+  storageBucket: FIREBASE_STORAGEBUCKET,
+  messagingSenderId: FIREBASE_MESSAGESENDERID,
+  appId: FIREBASE_APPID
 };
 
 // Initialize Firebase
