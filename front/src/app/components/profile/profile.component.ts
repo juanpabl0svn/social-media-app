506f3892ab32ce1c81749503d5f071ba3c3d4eb4
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-05-27 16:48:55 -0500

    going

--- front/src/app/components/profile/profile.component.ts
+++ front/src/app/components/profile/profile.component.ts
@@ -2,11 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import UserService from '../../services/user/user.service';
 import { RouterLink } from '@angular/router';
+import { HeaderComponent } from '../landing/header/header.component';
+
+
+
+
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, HeaderComponent],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.css'
 })
