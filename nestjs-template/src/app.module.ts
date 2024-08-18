93fd9c6757e8784bddd3647826d7b75c9839cd4a
Juan Pablo Sanchez Villegas <juanapablo@yconsultores.com>
2024-08-18 01:40:54 -0500

    notification module

--- nestjs-template/src/app.module.ts
+++ nestjs-template/src/app.module.ts
@@ -6,13 +6,14 @@ import { ChatModule } from './modules/socket/chat/chat.module';
 import { UserModule } from './modules/rest/user/user.module';
 import { PostModule } from './modules/rest/post/post.module';
 import { JwtModule } from '@nestjs/jwt';
+import { NotificationsModule } from './modules/rest/notifications/notifications.module';
 
 @Module({
   imports: [ConfigModule.forRoot({
     isGlobal: true,
     envFilePath: '.env'
   }),
-    ChatModule, UserModule, PostModule],
+    ChatModule, UserModule, PostModule, NotificationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
