39d176b7095ff62c4db3c27db2f91859e7ee9337
FlorezZ1025 <sanaraflorez@gmail.com>
2024-08-20 17:09:49 -0500

    cambios hechos en legislacion

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -3,6 +3,14 @@ module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
     extend: {
+      screens:{
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1536px",
+        'half':'50vh',
+      },
       colors: {
         "primary-color": "var(--primary-color)",
         "secondary-color": "var(--secondary-color)",
