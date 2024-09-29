0e47bffe53552c7493a2f45a45dead81fbe97001
Juan Pablo Sanchez <juanpablo@yconsultores.com>
2024-09-29 00:48:05 -0500

    last test today

--- front/src/app/views/profile/edit-profile/edit-profile.component.spec.ts
+++ front/src/app/views/profile/edit-profile/edit-profile.component.spec.ts
@@ -1,4 +1,44 @@
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { EditProfileComponent } from "./edit-profile.component";
+import { ToastrModule, ToastrService } from "ngx-toastr";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ActivatedRoute } from "@angular/router";
 
 
 
-describe()
+describe('Edit profile TEST', () => {
+
+    let component: EditProfileComponent;
+    let fixture: ComponentFixture<EditProfileComponent>;
+    let toast: ToastrService;
+
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [EditProfileComponent, HttpClientTestingModule, ToastrModule.forRoot()],
+            providers: [
+                {
+                    provide: ActivatedRoute,
+                    useValue: {
+                        snapshot: { paramMap: { get: (key: string) => 'test-param' } }
+                    }
+                },
+                ToastrService
+            ]
+        })
+            .compileComponents();
+
+        fixture = TestBed.createComponent(EditProfileComponent);
+        component = fixture.componentInstance;
+        toast = TestBed.inject(ToastrService);
+
+        spyOn(toast, 'error');
+        spyOn(toast, 'success');
+
+        fixture.detectChanges();
+    })
+
+
+    it('should be defined', () => {
+        expect(component).toBeDefined();
+    })
+})
